9d396fb36fdbfb057c1f0923a95554348d370a55
gdupont <ger.dupont@gmail.com>
2016-10-09 17:25:52 +0200

    minimun set of stuff for the interpreter

--- influxdb/src/main/java/org/apache/zeppelin/influxdb/InfluxdbInterpreter.java
+++ influxdb/src/main/java/org/apache/zeppelin/influxdb/InfluxdbInterpreter.java
@@ -14,324 +14,106 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.zeppelin.ignite;
+package org.apache.zeppelin.influxdb;
 
-import org.apache.ignite.Ignite;
-import org.apache.ignite.Ignition;
-import org.apache.ignite.configuration.IgniteConfiguration;
-import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
-import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.apache.zeppelin.interpreter.*;
-import org.apache.zeppelin.interpreter.InterpreterResult.Code;
 import org.apache.zeppelin.interpreter.thrift.InterpreterCompletion;
-import org.apache.zeppelin.scheduler.Scheduler;
-import org.apache.zeppelin.scheduler.SchedulerFactory;
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Query;
+import org.influxdb.dto.QueryResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.PrintWriter;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Properties;
-
-import scala.Console;
-import scala.None;
-import scala.Some;
-import scala.collection.JavaConversions;
-import scala.tools.nsc.Settings;
-import scala.tools.nsc.interpreter.IMain;
-import scala.tools.nsc.interpreter.Results.Result;
-import scala.tools.nsc.settings.MutableSettings.BooleanSetting;
-import scala.tools.nsc.settings.MutableSettings.PathSetting;
+import java.util.*;
 
 /**
- * Apache Ignite interpreter (http://ignite.incubator.apache.org/).
- *
+ * Influxdb interpreter (https://www.influxdata.com/time-series-platform/influxdb/).
+ * <p>
  * Use the following properties for interpreter configuration:
- *
+ * <p>
  * <ul>
- *     <li>{@code ignite.addresses} - coma separated list of hosts in form {@code <host>:<port>}
- *     or {@code <host>:<port_1>..<port_n>} </li>
- *     <li>{@code ignite.clientMode} - indicates that Ignite interpreter
- *     should start node in client mode ({@code true} or {@code false}).</li>
- *     <li>{@code ignite.peerClassLoadingEnabled} - enables/disables peer class loading
- *     ({@code true} or {@code false}).</li>
- *     <li>{@code ignite.config.url} - URL for Ignite configuration. If this URL specified then
- *     all aforementioned properties will not be taken in account.</li>
+ * <li>{@code Influxdb.addresses} - coma separated list of hosts in form {@code <host>:<port>}
+ * or {@code <host>:<port_1>..<port_n>} </li>
  * </ul>
  */
-public class IgniteInterpreter extends Interpreter {
-  static final String IGNITE_ADDRESSES = "ignite.addresses";
+public class InfluxdbInterpreter extends Interpreter {
+  private static Logger logger = LoggerFactory.getLogger(InfluxdbInterpreter.class);
 
-  static final String IGNITE_CLIENT_MODE = "ignite.clientMode";
+  protected static final List<String> COMMANDS = Arrays.asList(
+      "ALL", "ALTER", "ANY", "AS", "ASC", "BEGIN", "BY", "CREATE", "CONTINUOUS", "DATABASE",
+      "DATABASES", "DEFAULT", "DELETE", "DESC", "DESTINATIONS", "DIAGNOSTICS", "DISTINCT", "DROP",
+      "DURATION", "END", "EVERY", "EXPLAIN", "FIELD", "FOR", "FROM", "GRANT", "GRANTS", "GROUP",
+      "GROUPS", "IN", "INF", "INSERT", "INTO", "KEY", "KEYS", "KILL", "LIMIT", "SHOW",
+      "MEASUREMENT", "MEASUREMENTS", "NAME", "OFFSET", "ON", "ORDER", "PASSWORD", "POLICY",
+      "POLICIES", "PRIVILEGES", "QUERIES", "QUERY", "READ", "REPLICATION", "RESAMPLE", "RETENTION",
+      "REVOKE", "SELECT", "SERIES", "SET", "SHARD", "SHARDS", "SLIMIT", "SOFFSET", "STATS",
+      "SUBSCRIPTION", "SUBSCRIPTIONS", "TAG", "TO", "USER", "USERS", "VALUES", "WHERE", "WITH",
+      "WRITE");
 
-  static final String IGNITE_PEER_CLASS_LOADING_ENABLED = "ignite.peerClassLoadingEnabled";
+  public static final String INFLUXDB_HOST = "influxdb.host";
+  public static final String INFLUXDB_PORT = "influxdb.port";
+  public static final String INFLUXDB_USERNAME = "influxdb.username";
+  public static final String INFLUXDB_PASSWORD = "influxdb.password";
 
-  static final String IGNITE_CFG_URL = "ignite.config.url";
+  public static final String INFLUXDB_DBNAME = "influxdb.dbname";
 
-  static {
-    Interpreter.register(
-            "ignite",
-            "ignite",
-            IgniteInterpreter.class.getName(),
-            true,
-            new InterpreterPropertyBuilder()
-                    .add(IGNITE_ADDRESSES, "127.0.0.1:47500..47509",
-                            "Coma separated list of addresses "
-                                    + "(e.g. 127.0.0.1:47500 or 127.0.0.1:47500..47509)")
-                    .add(IGNITE_CLIENT_MODE, "true", "Client mode. true or false")
-                    .add(IGNITE_CFG_URL, "", "Configuration URL. Overrides all other settings.")
-                    .add(IGNITE_PEER_CLASS_LOADING_ENABLED, "true",
-                            "Peer class loading enabled. true or false")
-                    .build());
-  }
+  private String host = "localhost";
+  private int port = 8086;
+  private String username = "root";
+  private String password = "root";
+  private String dbname = "default";
 
-  private Logger logger = LoggerFactory.getLogger(IgniteInterpreter.class);
-  private Ignite ignite;
-  private ByteArrayOutputStream out;
-  private IMain imain;
-  private Throwable initEx;
+  private InfluxDB influxdbClient;
 
-  public IgniteInterpreter(Properties property) {
+  public InfluxdbInterpreter(Properties property) {
     super(property);
+    this.host = getProperty(INFLUXDB_HOST);
+    this.port = Integer.parseInt(getProperty(INFLUXDB_PORT));
+    this.username = getProperty(INFLUXDB_USERNAME);
+    this.password = getProperty(INFLUXDB_PASSWORD);
+
+    this.dbname = getProperty(INFLUXDB_DBNAME);
   }
 
   @Override
   public void open() {
-    Settings settings = new Settings();
-
-    URL[] urls = getClassloaderUrls();
-
-    // set classpath
-    PathSetting pathSettings = settings.classpath();
-    StringBuilder sb = new StringBuilder();
-
-    for (File f : currentClassPath()) {
-      if (sb.length() > 0) {
-        sb.append(File.pathSeparator);
-      }
-      sb.append(f.getAbsolutePath());
-    }
-
-    if (urls != null) {
-      for (URL u : urls) {
-        if (sb.length() > 0) {
-          sb.append(File.pathSeparator);
-        }
-        sb.append(u.getFile());
-      }
-    }
-
-    pathSettings.v_$eq(sb.toString());
-    settings.scala$tools$nsc$settings$ScalaSettings$_setter_$classpath_$eq(pathSettings);
-
-    settings.explicitParentLoader_$eq(new Some<>(Thread.currentThread().getContextClassLoader()));
-
-    BooleanSetting b = (BooleanSetting) settings.usejavacp();
-    b.v_$eq(true);
-    settings.scala$tools$nsc$settings$StandardScalaSettings$_setter_$usejavacp_$eq(b);
-
-    out = new ByteArrayOutputStream();
-    imain = new IMain(settings, new PrintWriter(out));
-
-    initIgnite();
+    this.influxdbClient = InfluxDBFactory.connect(this.host + ":" + this.port,
+        this.username, this.password);
   }
 
-  private List<File> currentClassPath() {
-    List<File> paths = classPath(Thread.currentThread().getContextClassLoader());
-    String[] cps = System.getProperty("java.class.path").split(File.pathSeparator);
-
-    for (String cp : cps) {
-      paths.add(new File(cp));
+  @Override
+  public InterpreterResult interpret(String influxdbQuery, InterpreterContext context) {
+    //prepare query
+    Query query = new Query(influxdbQuery, this.dbname);
+    //run it
+    QueryResult queryRes = influxdbClient.query(query);
+    if (queryRes == null) {
+      return new InterpreterResult(InterpreterResult.Code.ERROR, "Query result is empty.");
     }
-
-    return paths;
-  }
-
-  private List<File> classPath(ClassLoader cl) {
-    List<File> paths = new LinkedList<>();
-
-    if (cl == null) {
-      return paths;
+    if (queryRes.hasError()) {
+      return new InterpreterResult(InterpreterResult.Code.ERROR, queryRes.getError());
     }
-
-    if (cl instanceof URLClassLoader) {
-      URLClassLoader ucl = (URLClassLoader) cl;
-      URL[] urls = ucl.getURLs();
-      if (urls != null) {
-        for (URL url : urls) {
-          paths.add(new File(url.getFile()));
-        }
-      }
-    }
-
-    return paths;
-  }
-
-  public Object getLastObject() {
-    Object obj = imain.lastRequest().lineRep().call(
-        "$result",
-        JavaConversions.asScalaBuffer(new LinkedList<Object>()));
-    return obj;
-  }
-
-  private Ignite getIgnite() {
-    if (ignite == null) {
-      try {
-        String cfgUrl = getProperty(IGNITE_CFG_URL);
-
-        if (cfgUrl != null && !cfgUrl.isEmpty()) {
-          ignite = Ignition.start(new URL(cfgUrl));
-        } else {
-          IgniteConfiguration conf = new IgniteConfiguration();
-
-          conf.setClientMode(Boolean.parseBoolean(getProperty(IGNITE_CLIENT_MODE)));
-
-          TcpDiscoveryVmIpFinder ipFinder = new TcpDiscoveryVmIpFinder();
-          ipFinder.setAddresses(getAddresses());
-
-          TcpDiscoverySpi discoSpi = new TcpDiscoverySpi();
-          discoSpi.setIpFinder(ipFinder);
-          conf.setDiscoverySpi(discoSpi);
-
-          conf.setPeerClassLoadingEnabled(
-                  Boolean.parseBoolean(getProperty(IGNITE_PEER_CLASS_LOADING_ENABLED)));
-
-          ignite = Ignition.start(conf);
-        }
-
-        initEx = null;
-      } catch (Exception e) {
-        logger.error("Error in IgniteInterpreter while getIgnite: " , e);
-        initEx = e;
-      }
-    }
-    return ignite;
-  }
-
-  private void initIgnite() {
-    imain.interpret("@transient var _binder = new java.util.HashMap[String, Object]()");
-    Map<String, Object> binder = (Map<String, Object>) getLastObject();
-
-    if (getIgnite() != null) {
-      binder.put("ignite", ignite);
-
-      imain.interpret("@transient val ignite = "
-              + "_binder.get(\"ignite\")"
-              + ".asInstanceOf[org.apache.ignite.Ignite]");
-    }
+    //prepare result
+    return new InterpreterResult(
+        InterpreterResult.Code.SUCCESS,
+        InterpreterResult.Type.TEXT,
+        "" + queryRes);
   }
 
   @Override
   public void close() {
-    initEx = null;
-
-    if (ignite != null) {
-      ignite.close();
-      ignite = null;
-    }
-
-    if (imain != null) {
-      imain.close();
-      imain = null;
-    }
-  }
-
-  private List<String> getAddresses() {
-    String prop = getProperty(IGNITE_ADDRESSES);
-
-    if (prop == null || prop.isEmpty()) {
-      return Collections.emptyList();
-    }
-
-    String[] tokens = prop.split(",");
-    List<String> addresses = new ArrayList<>(tokens.length);
-    Collections.addAll(addresses, tokens);
-
-    return addresses;
-  }
-
-  @Override
-  public InterpreterResult interpret(String line, InterpreterContext context) {
-    if (initEx != null) {
-      return IgniteInterpreterUtils.buildErrorResult(initEx);
-    }
-
-    if (line == null || line.trim().length() == 0) {
-      return new InterpreterResult(Code.SUCCESS);
-    }
-
-    return interpret(line.split("\n"));
+    //nothing to do really
   }
 
   @Override
   public void cancel(InterpreterContext context) {
-  }
-
-  private InterpreterResult interpret(String[] lines) {
-    String[] linesToRun = new String[lines.length + 1];
-    System.arraycopy(lines, 0, linesToRun, 0, lines.length);
-    linesToRun[lines.length] = "print(\"\")";
-
-    Console.setOut(out);
-    out.reset();
-    Code code = null;
-
-    String incomplete = "";
-    for (int l = 0; l < linesToRun.length; l++) {
-      String s = linesToRun[l];      
-      // check if next line starts with "." (but not ".." or "./") it is treated as an invocation
-      if (l + 1 < linesToRun.length) {
-        String nextLine = linesToRun[l + 1].trim();
-        if (nextLine.startsWith(".") && !nextLine.startsWith("..") && !nextLine.startsWith("./")) {
-          incomplete += s + "\n";
-          continue;
-        }
-      }
-
-      try {
-        code = getResultCode(imain.interpret(incomplete + s));
-      } catch (Exception e) {
-        logger.info("Interpreter exception", e);
-        return new InterpreterResult(Code.ERROR, InterpreterUtils.getMostRelevantMessage(e));
-      }
-
-      if (code == Code.ERROR) {
-        return new InterpreterResult(code, out.toString());
-      } else if (code == Code.INCOMPLETE) {
-        incomplete += s + '\n';
-      } else {
-        incomplete = "";
-      }
-    }
-
-    if (code == Code.INCOMPLETE) {
-      return new InterpreterResult(code, "Incomplete expression");
-    } else {
-      return new InterpreterResult(code, out.toString());
-    }
-  }
-
-  private Code getResultCode(Result res) {
-    if (res instanceof scala.tools.nsc.interpreter.Results.Success$) {
-      return Code.SUCCESS;
-    } else if (res instanceof scala.tools.nsc.interpreter.Results.Incomplete$) {
-      return Code.INCOMPLETE;
-    } else {
-      return Code.ERROR;
-    }
+    //nothing to do really
   }
 
   @Override
   public FormType getFormType() {
-    return FormType.NATIVE;
+    return FormType.SIMPLE;
   }
 
   @Override
@@ -340,13 +122,14 @@ public class IgniteInterpreter extends Interpreter {
   }
 
   @Override
-  public List<InterpreterCompletion> completion(String buf, int cursor) {
-    return new LinkedList<>();
-  }
+  public List<InterpreterCompletion> completion(String s, int cursor) {
+    final List<InterpreterCompletion> suggestions = new ArrayList<InterpreterCompletion>();
 
-  @Override
-  public Scheduler getScheduler() {
-    return SchedulerFactory.singleton().createOrGetFIFOScheduler(
-            IgniteInterpreter.class.getName() + this.hashCode());
+    for (String cmd : COMMANDS) {
+      if (cmd.toUpperCase().contains(s)) {
+        suggestions.add(new InterpreterCompletion(cmd, cmd));
+      }
+    }
+    return suggestions;
   }
 }
--- influxdb/src/test/java/org/apache/zeppelin/influxdb/InfluxdbInterpreterTest.java
+++ influxdb/src/test/java/org/apache/zeppelin/influxdb/InfluxdbInterpreterTest.java
@@ -1,5 +1,48 @@
-package org.apache.zeppelin.interpreter.influxdb;
+package org.apache.zeppelin.influxdb;
 
-public Class InfluxdbInterpreterTest {
-    
-        }
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.BatchPoints;
+import org.influxdb.dto.Point;
+import org.influxdb.dto.Query;
+import org.influxdb.dto.QueryResult;
+
+import java.util.concurrent.TimeUnit;
+
+public class InfluxdbInterpreterTest {
+
+    public static void main(String[] args) {
+        InfluxDB influxDB = InfluxDBFactory.connect("http://localhost:8086", "root", "root");
+        String dbName = "aTimeSeries";
+        influxDB.createDatabase(dbName);
+
+        BatchPoints batchPoints = BatchPoints
+                .database(dbName)
+                .tag("async", "true")
+                .retentionPolicy("default")
+                .consistency(InfluxDB.ConsistencyLevel.ALL)
+                .build();
+        Point point1 = Point.measurement("cpu")
+                .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                .addField("idle", 90L)
+                .addField("user", 9L)
+                .addField("system", 1L)
+                .build();
+        Point point2 = Point.measurement("disk")
+                .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                .addField("used", 80L)
+                .addField("free", 1L)
+                .build();
+        batchPoints.point(point1);
+        batchPoints.point(point2);
+        influxDB.write(batchPoints);
+
+        Query query = new Query("SELECT idle FROM cpu", dbName);
+        QueryResult queryRes = influxDB.query(query);
+
+        System.out.println(queryRes);
+
+        influxDB.deleteDatabase(dbName);
+    }
+
+}
